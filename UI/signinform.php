85a18f59e8d9885f5026d12d5651fffe9c311c9a
Nikita <nikita.mantri21@gmail.com>
2014-05-22 18:14:56 +0530

    modified files

---
+++ UI/signinform.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>signup form</title>
+</head>
+<body>
+       <form action="insert.php" method="post">
+       		  student_id: <input type="text" name="Username"><br/>
+              Name: <input type="text" name="Name"><br />
+              Email_id: <input type="text" name="Email"><br/>
+              password: <input type="password" name="Password"><br />
+              <input type="submit">
+       </form>
+   </body>
+</html>
