f5f2d0adda19844b262e52456ad2396eda1b65dd
NatashaGarg <natsrock24@gmail.com>
2014-05-24 16:51:58 +0530

    connectivity.php added

---
+++ UI/connectivity.php
@@ -0,0 +1,40 @@
+<?php include_once("db.php");
+	  session_start();
+?>
+
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'bsi');
+define('DB_USER','root');
+define('DB_PASSWORD','vaio');
+
+/*
+$ID = $_POST['user'];
+$Password = $_POST['pass'];
+*/
+function SignIn()
+{
+session_start();   //starting the session for user profile page
+if(!empty($_POST['user']))   //checking the 'user' name which is from Sign-In.html, is it empty or have some text
+{
+	$query = mysql_query("SELECT *  FROM student_details where student_id = '$_POST['username']' AND password = '$_POST[password]'") or die(mysql_error());
+	$row = mysql_fetch_array($query) or die(mysql_error());
+	if(!empty($row['student_id']) AND !empty($row['password']))
+	{
+		$_SESSION['student_id'] = $row['password'];
+		echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
+
+	}
+	else
+	{
+		echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY...";
+	}
+}
+}
+if(isset($_POST['submit']))
+{
+	SignIn();
+}
+
+?>
+
