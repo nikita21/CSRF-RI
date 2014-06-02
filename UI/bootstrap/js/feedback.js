2bb7e2715f2def7c7ade32fffd7f62af4c48065d
NatashaGarg <natsrock24@gmail.com>
2014-06-03 01:45:38 +0530

    feedback.html

--- UI/bootstrap/js/feedback.js
+++ UI/bootstrap/js/feedback.js
@@ -1,6 +1,6 @@
 $(document).on("click", ".open-AddCourseDialog", function () {
      var myCourseId = $(this).data('id');
-     $(".modal-body #courseId").val( myCourseId );
+     $(".modal-dialog #courseId").val( myCourseId );
      // As pointed out in comments, 
      // it is superfluous to have to manually call the modal.
      // $('#addBookDialog').modal('show');
